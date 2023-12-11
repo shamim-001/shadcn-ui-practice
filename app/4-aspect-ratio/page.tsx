0cd3a2c0bb6977aa05483a5fe25d075327fc5b18
shamim-001 <s.ahsan.ds96@gmail.com>
2023-12-11 21:23:34 +0600

    skeleton in aspect ratio

--- app/4-aspect-ratio/page.tsx
+++ app/4-aspect-ratio/page.tsx
@@ -1,9 +1,14 @@
+"use client";
 import Image from "next/image";
 
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import Link from "next/link";
+import { useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Page = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const handleLoad = () => setIsLoaded(true);
   return (
     <div>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
@@ -12,11 +17,13 @@ const Page = () => {
 
       <div className="w-[600px]">
         <AspectRatio ratio={16 / 9}>
+          {!isLoaded && <Skeleton className="h-full w-full" />}
           <Image
             src="/aspect-ratio-bg.jpg"
             alt="Image"
             className="rounded-md object-cover"
             fill
+            onLoad={handleLoad}
           />
         </AspectRatio>
         <Link
